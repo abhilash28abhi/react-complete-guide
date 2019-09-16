9e6d691bf07973a03af11ccdb71f8f4cbbcd15df
Abhilash <abhilash.@Abhilashs-MacBook-Pro.local>
2019-09-16 20:15:12 +0530

    Changed the structure

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         persons: [
             { name: 'Abhilash', age: 28 },
             { name: 'Monika', age: 26 },
-            { name: 'Yusuf', age: 16 },
+            { name: 'David', age: 16 },
         ]
     };
 
@@ -18,7 +18,7 @@ class App extends Component {
             persons: [
                 { name: newName, age: 28 },
                 { name: 'Monika', age: 25 },
-                { name: 'State Changed', age: 16 },
+                { name: 'David', age: 16 },
             ]
         });
     }
@@ -28,7 +28,7 @@ class App extends Component {
             persons: [
                 { name: event.target.value, age: 28 },
                 { name: 'Monika', age: 25 },
-                { name: 'State Changed', age: 16 },
+                { name: 'David', age: 16 },
             ]
         });
     }
@@ -42,18 +42,14 @@ class App extends Component {
             cursor: 'pointer'
         };
 
-        return (<div className = "App" >
-            <h1 > Hi I am a React App by Abhilash from India!!! </h1> <button style = { buttonStyle }
-            onClick = { this.switchButtonHandler.bind(this, 'Anku') } > Switch name </button> <
-            Person name = { this.state.persons[0].name }
-            age = { this.state.persons[0].age }
-            changed = { this.nameChangedHandler }
-            /> 
-            <Person name = { this.state.persons[2].name }
-            age = { this.state.persons[2].age }
-            click = { this.switchButtonHandler.bind(this, 'Abhilash!!!') } > My Hobbies are racing </Person> 
-            <Person name = "Monika" age = "25" />
-            </div>
+        return (
+        <div className = "App" >
+            <h1 > Hi I am a React App by Abhilash from India!!! </h1> 
+            <button style = { buttonStyle } onClick = { this.switchButtonHandler.bind(this, 'Anku') } > Switch name </button> 
+            <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age } changed = { this.nameChangedHandler }/> 
+            <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age } click = { this.switchButtonHandler.bind(this, 'Abhilash!!!') } > My Hobbies are racing </Person> 
+            <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age } />
+        </div>
         );
         //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi I am a React App!!'));
     }
