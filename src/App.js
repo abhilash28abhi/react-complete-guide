5a0f9393ea5c73147fe1c0cf39645bd4979c4b89
Abhilash <abhilash.@Abhilashs-MacBook-Pro.local>
2019-09-17 22:51:59 +0530

    Delete person changes

--- src/App.js
+++ src/App.js
@@ -41,6 +41,14 @@ class App extends Component {
         });
     }
 
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+        persons.splice(personIndex,1);
+        this.setState({
+            persons : persons
+        });
+    }
+
     render() {
         const buttonStyle = {
             backgroundColor: 'white',
@@ -55,9 +63,9 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person name = { this.state.persons[0].name } age = { this.state.persons[0].age } changed = { this.nameChangedHandler }/> 
-                    <Person name = { this.state.persons[1].name } age = { this.state.persons[1].age } click = { this.switchButtonHandler.bind(this, 'Abhilash!!!') } > My Hobbies are racing </Person> 
-                    <Person name = { this.state.persons[2].name } age = { this.state.persons[2].age } />
+                    {this.state.persons.map((person, index) => {
+                       return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age}></Person>
+                    })}
                 </div>      
             );
         }
